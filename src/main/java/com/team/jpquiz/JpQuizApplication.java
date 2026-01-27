a8891ef879cb2a081ce0a2c814cc18bbf8a285e2
zkfmak9257 <zkfmak9257@naver.com>
2026-01-27 20:13:44 +0900

    feat/ksh/jpquizapplication앱-부팅-가능-상태구축 - SpringBootApplication 어노테이션 추가 - Mapper와 XML 기본 세팅

--- src/main/java/com/team/jpquiz/JpQuizApplication.java
+++ src/main/java/com/team/jpquiz/JpQuizApplication.java
@@ -1,4 +1,11 @@
 package com.team.jpquiz;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class JpQuizApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JpQuizApplication.class, args);
+    }
 }
--- src/main/java/com/team/jpquiz/member/query/infrastructure/MemberMapper.java
+++ src/main/java/com/team/jpquiz/member/query/infrastructure/MemberMapper.java
@@ -1,4 +1,7 @@
 package com.team.jpquiz.member.query.infrastructure;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface MemberMapper {
 }
