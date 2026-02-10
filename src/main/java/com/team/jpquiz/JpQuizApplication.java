7810c2c533c956c5bbe6e13e1e5fcb17cd4c356f
wjdqudwls <wjdqudwls100@gmail.com>
2026-02-11 01:25:50 +0900

    pr 리뷰 코멘트 반영

--- src/main/java/com/team/jpquiz/JpQuizApplication.java
+++ src/main/java/com/team/jpquiz/JpQuizApplication.java
@@ -2,7 +2,9 @@ package com.team.jpquiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class JpQuizApplication {
     public static void main(String[] args) {
