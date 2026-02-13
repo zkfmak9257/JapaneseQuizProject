9756dbbc1442dcaf29960cb41780b8da2f2fbd48
zkfmak9257 <zkfmak9257@naver.com>
2026-02-13 16:47:55 +0900

    feat/ksh/API스펙 정의

--- src/main/java/com/team/jpquiz/quiz/dto/request/StartQuizRequest.java
+++ src/main/java/com/team/jpquiz/quiz/dto/request/StartQuizRequest.java
@@ -1,4 +1,15 @@
 package com.team.jpquiz.quiz.dto.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class StartQuizRequest {
+    @Min(value = 1, message = "문제 수는 1 이상이어야 합니다.")
+    @Max(value = 20, message = "문제 수는 20 이하여야 합니다.")
+    private Integer count;
+
 }
