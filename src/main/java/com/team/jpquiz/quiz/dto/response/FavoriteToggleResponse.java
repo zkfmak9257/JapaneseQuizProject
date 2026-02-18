77a67ab6595fa590afd67e18bdd2039ff8a011d4
wjdqudwls <wjdqudwls100@gmail.com>
2026-02-18 15:35:13 +0900

    [Feat] 즐겨찾기 토글 API 추가 및 조회 경로 정합성 반영

---
+++ src/main/java/com/team/jpquiz/quiz/dto/response/FavoriteToggleResponse.java
@@ -0,0 +1,16 @@
+package com.team.jpquiz.quiz.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FavoriteToggleResponse {
+
+    private Long questionId;
+    private boolean favorited;
+}
